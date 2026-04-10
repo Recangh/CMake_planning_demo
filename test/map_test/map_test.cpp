afeda2565b4f014fc1cb6249717ff266374113d6
Recangh <wjx060916240607@qq.com>
2026-04-10 16:35:14 +0800

    add test demo

---
+++ test/map_test/map_test.cpp
@@ -0,0 +1,14 @@
+#include "pnc_map.h"
+#include <iostream>
+using namespace std;
+
+void mapTest(){
+    cout << "This is a pnc_map test." << endl;
+    PNC_Map map;
+    map.mapInfo();
+}
+
+int main(){
+    mapTest();
+    return 0;
+}
